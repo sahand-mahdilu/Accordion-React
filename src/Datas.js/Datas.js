84f39021fb897677f49a24230857967a83c5b8a8
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-17 17:48:21 +0330

    feat : datas created

---
+++ src/Datas.js/Datas.js
@@ -0,0 +1,12 @@
+
+let Datas=[
+    {id:1,title:"What is javaScript?",info:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, molestiae set"},
+    {id:2,title:"What is React?",info:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, molestiae set"},
+    {id:3,title:"What is HTML?",info:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, molestiae set"},
+    {id:4,title:"What is API?",info:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, molestiae set"},
+    {id:5,title:"What is CSS?",info:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, molestiae set"},
+
+    
+]
+
+
