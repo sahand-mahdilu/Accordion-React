f3e4bc90d24394d25cd23de1d81c0ada58688e1d
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-18 09:54:12 +0330

    fix : showing info fixed

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -3,43 +3,27 @@ import { Datas } from '../Datas.js/Datas'
 
 export default function Question() {
 
-    
-    const [Question,setQusetion]=useState(Datas)
-    const [isShowing,setIsshowing]=useState(false)
+    const [questions, setQuestions] = useState(Datas.map(data => ({ ...data, isShowing: false })))
 
-    console.log(Question);//array
-
-    const showInfo= ()=>{
-
-        setIsshowing(prev=>!prev)
-
+    const toggleInfo = (id) => {
+        setQuestions(prevQuestions =>
+            prevQuestions.map(question =>
+                question.id === id ? { ...question, isShowing: !question.isShowing } : question
+            )
+        )
     }
 
-
-  return (
-
-    
-    <div className='w-[60%] flex justify-between mt-36 bg-white p-4'>
-
-        
-        <h1 className='font-bold'>Qusetions and Answers <br /> About programming</h1>
-
-        <div className='flex flex-col gap-2 w-full  p-1'>
-            {Question.map(q=>{
-                return(
-                    <div onClick={showInfo} key={q.id} className=' rounded-md p-2 bg-yellow-200 w-full flex flex-col '>
-             
-                 <p>{q.title}</p>
-
-                 {isShowing &&  <p>{q.info}</p>} 
-                 
-               
+    return (
+        <div className='w-[60%] flex justify-between mt-36 bg-white p-4'>
+            <h1 className='font-bold'>Questions and Answers <br /> About Programming</h1>
+            <div className='flex flex-col gap-2 w-full p-1'>
+                {questions.map(q => (
+                    <div onClick={() => toggleInfo(q.id)} key={q.id} className='rounded-md p-2 bg-yellow-200 w-full flex flex-col cursor-pointer'>
+                        <p>{q.title}</p>
+                        {q.isShowing && <p>{q.info}</p>}
+                    </div>
+                ))}
             </div>
-                )
-            })}
-            
         </div>
-
-    </div>
-  )
+    )
 }
